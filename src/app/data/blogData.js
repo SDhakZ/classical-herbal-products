7962a85dfb94429009e1f60cf594c36b32866d0a
Sharnam <sharnam34@gmail.com>
2024-08-16 17:01:40 +0545

    Blog data structuring

--- src/app/data/blogData.js
+++ src/app/data/blogData.js
@@ -6,6 +6,55 @@ export const blogData = [
     brief:
       "Shilajit is a sticky substance that comes out of the cracks in high mountain ranges of Asia. It's made of animal, mineral, and plant remains.",
     image: "shilajit-benefits.png",
+    body: [
+      {
+        title: "4 Benefit of Shilajit",
+        description:
+          "Shilajit, also known as mumiyo or mumie, is an organic mineral compound found in the Himalayan mountains and mountainous regions in Russia, Afghanistan, and elsewhere. This naturally occurring, blackish-brown resin contains over 40 minerals and substances, most notably fulvic acid.1",
+        subBody: [
+          {
+            title: "1. Has Anti-Inflammatory and Antioxidant Effects",
+            description:
+              "Shilajit has strong anti-inflammatory and antioxidant effects. The compound is thought to promote greater longevity and have anti-aging properties in Ayurvedic medicine. These benefits can support overall health and reduce the risk of certain conditions.",
+            bullet: {
+              brief: "Research has shown that shilajit may also:",
+              points: [
+                "Increase cell turnover",
+                "Potentially encourage the body to produce more collagen",
+                "Slow down oxidative stress",
+              ],
+            },
+          },
+          {
+            title: "2. May Prevent Bone Loss",
+            description:
+              "Shilajit may have a positive effect on bone health. A study published in 2022 found postmenopausal women with osteopenia (low bone mineral density) who took shilajit supplements daily saw significant improvements in bone density after 11 months. The researchers theorized that this effect may be due to shilajit's ability to speed up cell turnover.",
+          },
+          {
+            title: "3. Could Increase Male Fertility",
+            description:
+              "Shilajit may increase male fertility by boosting key hormones. Research has shown that taking 500 milligrams (mg) of shilajit per day can significantly increased dehydroepiandrosterone (DHEAS) and total testosterone levels.",
+          },
+          {
+            title: "4. Might Improve Exercise Performance",
+            description:
+              "A growing body of research indicates that shilajit can be helpful for exercise efforts. Some evidence suggests that athletes have less post-workout fatigue and are better able to reach peak muscular strength after taking shilajit supplements.",
+          },
+        ],
+      },
+      {
+        title: "How to take Shilajit?",
+        description:
+          "You can find shilajit supplements at many online shops, pharmacies, drugstores, and health food stores. It's available as a dissolvable powder for drinks like water or milk and as a capsule.",
+        subBody: [
+          {
+            title: "Dosage",
+            description:
+              "Shilajit may have a positive effect on bone health. A study published in 2022 found postmenopausal women with osteopenia (low bone mineral density) who took shilajit supplements daily saw significant improvements in bone density after 11 months. The researchers theorized that this effect may be due to shilajit's ability to speed up cell turnover.",
+          },
+        ],
+      },
+    ],
   },
   {
     id: 2,
